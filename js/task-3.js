f7d8fb4476a3b52012e16d6bca81f464048fdb09
Usamovara <65997618+usamovara@users.noreply.github.com>
2024-02-20 18:26:25 +0200

    goit-js-hw-02
    initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function checkForSpam(message){
+    message=message.toLowerCase();
+
+ if((message.indexOf("spam")<0) && (message.indexOf("sale")<0)){
+    return false;
+ }else 
+    return true;
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
